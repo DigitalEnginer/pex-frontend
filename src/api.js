47299ccf1d4f3b8c11d0f2e4221e3bae5ed3742c
DigitalEnginer <ernurramazanov060920@gmail.com>
2026-05-01 14:00:35 +0500

    update api url

--- src/api.js
+++ src/api.js
@@ -1,15 +1,22 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // URL твоего бэкенда на Render (строго без слэша на конце)
   baseURL: 'https://pex-backend-vtex.onrender.com'
 });
 
-api.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+// Автоматически прикрепляем токен к каждому запросу (если юзер залогинен)
+api.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default api;
